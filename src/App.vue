71be8c95c221c56f5e47a043bcb50666bcd6fc51
miliberlin <michelle.liebheit@posteo.de>
2022-11-22 11:37:26 +0100

    vue.js, vite, tailwind setup

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+</script>
+
+<template>
+  <div>Hello</div>
+</template>
+
+<style scoped>
+
+</style>
